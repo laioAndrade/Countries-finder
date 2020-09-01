6e2f4548ae9dea9843da8c993ba10c611a6eca0f
Laio Andrade <laioand98@gmail.com>
2020-09-01 19:10:12 -0300

    Adding live search function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Toolbar from './Toolbar/Toolbar';
 
 import axios from './axios';
 import CardArea from './CardArea/CardArea';
+import SearchTools from './SearchTools/SearchTools';
 
 class App extends Component {
   state = {
@@ -14,17 +15,37 @@ class App extends Component {
   componentDidMount() {
     axios.get('/all')
       .then(response => {
-        const countries = response.data;
-        this.setState({countries: countries});
-        console.log(response);
+        const updateCountries = response.data;
+        this.setState({countries: updateCountries});
       });
   } 
+
+  textHandler = (event) => {
+    if(event.target.value){
+      axios.get('/name/' + event.target.value)
+      .then(response => {
+        const updateCountries = response.data;
+        this.setState({countries: updateCountries})
+      })
+      .catch(error => {
+        console.log('deu ruim');
+      });
+    } else {
+        axios.get('/all')
+        .then(response => {
+          const updateCountries = response.data;
+          this.setState({countries: updateCountries});
+        });
+    }
+    
+}
 
   render() {
 
     return (
       <div >
           <Toolbar />
+          <SearchTools changed={this.textHandler}/>
           <CardArea countries={this.state.countries}/>
       </div>
     );
