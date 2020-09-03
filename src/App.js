7212148b387078c83c78b56a8e2c4fbfa676c025
Laio Andrade <laioand98@gmail.com>
2020-09-03 19:08:44 -0300

    add router & change function from App to CardArea

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Toolbar from './Toolbar/Toolbar';
-
-import axios from './axios';
 import CardArea from './CardArea/CardArea';
 import SearchTools from './SearchTools/SearchTools';
+
+import axios from './axios';
+
+import { Route } from 'react-router-dom';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -14,58 +16,13 @@ import { faMoon, faSearch} from '@fortawesome/free-solid-svg-icons';
 library.add(fab, faMoon, faSearch);
 
 class App extends Component {
-  state = {
-    countries: [],
-  }
-
-  componentDidMount() {
-    this.getAll();
-  } 
-
-
-  getAll () {
-    axios.get('/all')
-    .then(response => {
-      const updateCountries = response.data;
-      this.setState({countries: updateCountries});
-    });
-  }
-
-  inputTextHandler = (event) => {
-    if(event.target.value){
-      axios.get('/name/' + event.target.value)
-      .then(response => {
-        const updateCountries = response.data;
-        this.setState({countries: updateCountries})
-      })
-      .catch(error => {
-        this.setState({countries: []})
-      });
-    } else {
-        this.getAll();
-    }  
-  }
-
-  selectHandler = (event) => {
-    if(event.target.value === ""){
-      this.getAll();
-    }else {
-      axios.get('/region/' + event.target.value)
-      .then(response => {
-        const updateCountries = response.data;
-        this.setState({countries: updateCountries});
-      });
-    }
-    
-  }
-
+  
   render() {
 
     return (
       <div >
           <Toolbar />
-          <SearchTools inputChanged={this.inputTextHandler} selectChanged={this.selectHandler}/>
-          <CardArea countries={this.state.countries}/>
+          <Route path="/" component={CardArea}/>
       </div>
     );
   }
--- src/CardArea/CardArea.js
+++ src/CardArea/CardArea.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
 import classes from './CardArea.module.css';
 
+import axios from '../axios';
+
 import Card from './Card/Card';
+import SearchTools from '../SearchTools/SearchTools';
 
 class CardArea extends Component {
+    state = {
+        countries: [],
+      }
+    
+      getAll () {
+        axios.get('/all')
+        .then(response => {
+          const updateCountries = response.data;
+          this.setState({countries: updateCountries});
+        });
+      }
+    
+      componentDidMount() {
+        this.getAll();
+      } 
+    
+      inputTextHandler = (event) => {
+        if(event.target.value){
+          axios.get('/name/' + event.target.value)
+          .then(response => {
+            const updateCountries = response.data;
+            this.setState({countries: updateCountries})
+          })
+          .catch(error => {
+            this.setState({countries: []})
+          });
+        } else {
+            this.getAll();
+        }  
+      }
+    
+      selectHandler = (event) => {
+        if(event.target.value === ""){
+          this.getAll();
+        }else {
+          axios.get('/region/' + event.target.value)
+          .then(response => {
+            const updateCountries = response.data;
+            this.setState({countries: updateCountries});
+          });
+        }
+        
+      }
+    
 
     render() {
-        const countries = this.props.countries.map(country => {
+        const countries = this.state.countries.map(country => {
             return <Card 
               key={country.name}
               name={country.name} 
@@ -17,10 +64,12 @@ class CardArea extends Component {
               />
         })
         return(
+            <>
+            <SearchTools inputChanged={this.inputTextHandler} selectChanged={this.selectHandler}/>
             <div className={classes.CardArea}>
                 {countries}
             </div>
-            
+            </>
         );
     }
 }
