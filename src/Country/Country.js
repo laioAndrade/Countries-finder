1a1a12c68cb82e74b2ff1907a53b579a126c86c9
Laio Andrade <laioand98@gmail.com>
2020-09-03 22:28:06 -0300

    add request in component

--- src/Country/Country.js
+++ src/Country/Country.js
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Country.module.css';
 
-const country = (props) => {
-    return (
-        <div className={classes.Country}> 
-            <div className={classes.Flag}><img src={props.location.state.flag}/></div>
-            <div className={classes.Info}>
-                <p style={{fontWeight: 'bold', fontSize: '1.6rem'}}>{props.location.state.name}</p>
-                <div className={classes.InfoSides}>
-                    <div className={classes.InfoLeft}>
-                        <span>Native Name: {props.location.state.nativeName}</span>
-                        <span>Population: {props.location.state.population}</span>
-                        <span>Region: {props.location.state.region}</span>
-                        <span>Sub Region: {props.location.state.subregion}</span>
-                        <span>Capital: {props.location.state.capital}</span>
-                    </div>
-                    <div className={classes.InfoRight}>
-                        <span>Top Level Domain: {props.location.state.topLevelDomain}</span>
-                        <span>Languages</span>
+import axios from 'axios';
+import { Redirect } from 'react-router';
+import CardArea from '../CardArea/CardArea';
+
+class Country extends Component {
+    state = {
+        country: [],
+        error: false
+    }
+
+    componentDidMount() {      
+            axios.get('https://restcountries.eu/rest/v2/name/' + this.props.match.params.name + '?fullText=true')
+            .then(response => {
+                this.setState({country: response.data[0]});
+            })
+            .catch(error => {
+                this.setState({error: true});
+            });
+    }
+
+    render () {
+         
+        return (
+            !this.state.error ?  <div className={classes.Country}> 
+                <div className={classes.Flag}><img className={classes.Img} src={this.state.country.flag} alt="Flag"/></div>
+                <div className={classes.Info}>
+                    <p style={{fontWeight: 'bold', fontSize: '1.6rem'}}>{this.state.country.name}</p>
+                    <div className={classes.InfoSides}>
+                        <div className={classes.InfoLeft}>
+                            <span>Native Name: {this.state.country.nativeName}</span>
+                            <span>Population: {this.state.country.population}</span>
+                            <span>Region: {this.state.country.region}</span>
+                            <span>Sub Region: {this.state.country.subregion}</span>
+                            <span>Capital: {this.state.country.capital}</span>
+                        </div>
+                        <div className={classes.InfoRight}>
+                            <span>Top Level Domain: {this.state.country.topLevelDomain}</span>
+                            <span>Languages</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+            : <Redirect from={this.props.match.url} to={CardArea}/> 
+            
+        );
+    }
+
+    
 }
 
-export default country;
+export default Country;
